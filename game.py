2e5e2ba6ed318bb58e5244fe8dd01b5e69997b61
keshar singh sunar <kesharsunar77@gmail.com>
2025-02-25 14:57:47 +0545

    santosh updated

--- game.py
+++ game.py
@@ -48,7 +48,7 @@ class Game:
             'grass': load_images('tiles/grass'),
             'large_decor': load_images('tiles/large_decor'),
             'stone': load_images('tiles/stone'),
-            'player': load_image('entities/player.png'),
+            'player': load_image('entities/player.png') ,
             'background': load_image('background.png'),
             'clouds': load_images('clouds'),
             'enemy/idle': Animation(load_images('entities/enemy/idle'),img_dur = 6),
